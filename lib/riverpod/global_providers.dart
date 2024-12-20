c5d2470eaf47a8d6d4b34573f0ffa679b9aebb6c
PoorneshPR <poorneshpr@gmail.com>
2024-12-20 12:04:29 +0530

    documentation on read me added and changed file structure

--- lib/riverpod/global_providers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sample_riverpod/riverpod/counter/counter_provider.dart';
-import 'package:sample_riverpod/riverpod/counter/counter_state.dart';
-
-final counterGlobalProvider =
-    NotifierProvider<CounterProvider, CounterState>(CounterProvider.new);
